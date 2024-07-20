bc6bc28c7c8a25ff2d477e1af5acae64a337f83e
Tyler Weeres <tweeres04@gmail.com>
2024-07-19 18:16:04 -0700

    Use a table for temperature history

--- site/app/routes/_index.tsx
+++ site/app/routes/_index.tsx
@@ -43,9 +43,10 @@ function LatestEntry({ entry }: { entry: Entry }) {
 
 function Entry({ entry }: { entry: Entry }) {
 	return (
-		<li>
-			{formatDate(entry.timestamp)} - {entry.value}°C
-		</li>
+		<tr>
+			<td>{formatDate(entry.timestamp)}</td>
+			<td className="text-right">{entry.value}°C</td>
+		</tr>
 	)
 }
 
@@ -55,15 +56,21 @@ export default function Index() {
 	const [firstEntry, ...restOfEntries] = entries
 
 	return (
-		<div className="font-sans p-4 container mx-auto">
+		<div className="font-sans p-4 max-w-[500px] lg:max-w-[750px] mx-auto">
 			<h1 className="text-3xl mb-3">Ty&apos;s Raspberry Pi</h1>
 			<LatestEntry entry={firstEntry} />
 			<h2 className="text-2xl mb-3">Temp history</h2>
-			<ul className="space-y-1">
+			<table className="w-full">
+				<thead>
+					<tr>
+						<th>Timestamp</th>
+						<th className="text-right">Temperature</th>
+					</tr>
+				</thead>
 				{restOfEntries.map((e: Entry) => (
 					<Entry entry={e} key={e.id} />
 				))}
-			</ul>
+			</table>
 		</div>
 	)
 }
