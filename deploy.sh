d46fa45a7d289f75370d54cdbdc6597b15a1bd0d
Tyler Weeres <tweeres04@gmail.com>
2024-07-19 17:03:53 -0700

    Try to separate package.json out; Try removing docker compose down

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 ssh raspberrypi -T <<'EOL'
 	cd raspberrypi && \
 	git fetch && git reset --hard origin/main && \
-	sudo docker compose -f docker/compose.yml down
 	sudo docker compose -f docker/compose.yml up --build -d
 EOL
